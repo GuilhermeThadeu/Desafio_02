04538bcf6b49df8f25e46f44e01eab94f65038f1
Guilherme Thadeu <guilhermetadeuh@hotmail.com>
2021-12-07 00:05:07 -0300

    T1: utilizei uma variavel mais soma.
    T2: usei console.writeline para agrupar os nomes.
    T3: criei variaves para converter e calcular o imc.
    T4: usei if para me dizer se era true ou false e uma variavel para somar a quantidade de dev.
    T5: criei uma string para buscar todos os nomes com Silva.

--- Program.cs
+++ Program.cs
@@ -30,16 +30,65 @@ namespace Desafio_02
 
 
             //T1 - Somar a idade de todos Ubuntus
-
+            int c = idade1 + idade2 + idade3;
+            Console.WriteLine($"idade total dos Ubuntus: {c} anos ");
+            Console.WriteLine();
             //T2 - Agrupar os nomes de todos Ubuntus
-
+             Console.WriteLine($"Todos os nomes dos Ubuntus:\n {nome1}\n {nome2}\n {nome3}\n ");
             //T3 - Calcular Média dos IMC de todos Ubuntus
+            var imcaltura1 = Convert.ToInt32(altura1);
+            var imcpeso1 = Convert.ToInt32(peso1);
+            var imc1 = imcpeso1 / (imcaltura1 * imcaltura1);
+            Console.WriteLine($"media de IMC do {nome1}: {imc1}");
 
+            var imcaltura2 = Convert.ToInt32(altura2);
+            var imcpeso2 = Convert.ToInt32(peso2);
+            var imc2 = imcpeso1 / (imcaltura2 * imcaltura2);
+            Console.WriteLine($"media de IMC do {nome2}: {imc2}");
+
+            var imcaltura3 = Convert.ToInt32(altura3);
+            var imcpeso3 = Convert.ToInt32(peso3);
+            var imc3 = imcpeso3 / (imcaltura3 * imcaltura3);
+            Console.WriteLine($"media de IMC do {nome1}: {imc3}");
+            Console.WriteLine();
             //T4 - Contar quantos Ubuntus sao Devs
+            int devcount = 0;
+            if (EhDev1 == true)
+            {
+                devcount++;
+            }
+
+            if   (EhDev2 == true)
+            {
+                devcount++;
+            }
+          
+            if (EhDev3 == true)
+            {
+                devcount++;
+            }
+
+            Console.WriteLine($"quantidade de ubuntu que sao devs: {devcount}");
+
+            Console.WriteLine();
 
             //T5 - Exibir somente Ubuntu com nome Silva
-
-
+            string[] vetnome;
+            vetnome = nome1.Split(" ");
+            if (vetnome[1] == "Silva")
+            {
+                Console.WriteLine(nome1);
+            }
+            vetnome = nome2.Split(" ");
+            if (vetnome[1] == "Silva")
+            {
+                Console.WriteLine(nome2);
+            }
+            vetnome = nome3.Split(" ");
+            if (vetnome[1] == "Silva")
+            {
+                Console.WriteLine(nome3);
+            }
 
             //Imprime na tela
             Console.WriteLine("UBUNTU 002 - ENDS...");
